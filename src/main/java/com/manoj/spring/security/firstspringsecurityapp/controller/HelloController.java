a2856e3ff10e008ffc4570ff8496b6966059f43a
Manoj-t <teluguntlamanoj@gmail.com>
2023-04-27 22:06:40 -0500

    Spring Security - Created an InMemoryUser and Custom Security Filter Chain

--- src/main/java/com/manoj/spring/security/firstspringsecurityapp/controller/HelloController.java
+++ src/main/java/com/manoj/spring/security/firstspringsecurityapp/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Spring Security rocks!!";
+        return "Spring Security Rocks!!";
     }
 }
---
+++ src/main/java/com/manoj/spring/security/firstspringsecurityapp/security/MySecurityConfig.java
@@ -0,0 +1,58 @@
+package com.manoj.spring.security.firstspringsecurityapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class MySecurityConfig {
+
+
+    // expose bean called UserDetailsService so that Spring will run use it in the authentication filter chain
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        // creating our own User details manager
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        // Creating user with details
+        UserDetails userDetails = User.withUsername("Tom")
+                .password(passwordEncoder().encode("Cruise") )
+                .authorities("READ")
+                .build();
+
+        // assigning the new user to our own user details manager
+        userDetailsService.createUser(userDetails);
+
+        // return our own User details manager
+        return userDetailsService;
+    }
+
+   /*
+    Bean of BCryptPasswordEncoder to encode the passcode in the in-memory database.
+    when the request comes in Spring Security will look at this bean and it will use this bean on the incoming password
+    and then compare the passwords.
+    */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.httpBasic(); // Authenticate with Http Basic authentication
+
+        httpSecurity.authorizeHttpRequests().anyRequest().authenticated(); // Authorize any request that is authenticated.
+
+        return httpSecurity.build();
+
+    }
+
+}
